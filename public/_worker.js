1ece12674c4351bcaf3fcb7c5fd5c7ed561d68c3
placebetsai <placebetsai@users.noreply.github.com>
2026-08-29 22:32:22 -0400

    fix: remove broken _worker.js - Pages serves static files without it

--- public/_worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname === '/') {
-      return env.ASSET.fetch(new Request(new URL('/index.html', url), request));
-    }
-    return env.ASSET.fetch(request);
-  }
-};
